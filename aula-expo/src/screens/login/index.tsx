f01ea9a6cc05815702ee50a794136e964f1b2cc1
tchow2020 <lgmds2017@outlook.com>
2022-03-10 19:18:03 -0300

    desafio_feito

---
+++ aula-expo/src/screens/login/index.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { ImageBackground, Text, View } from 'react-native';
+
+
+
+
+
+export interface LoginscreenProps { 
+
+}
+
+
+export function Loginscreen (props: LoginscreenProps){
+
+
+    <input type="text" />
+
+}
+
+
